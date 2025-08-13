cf1480033740c7de7a59cfa3c4fb3e107552c783
minji-kim0524 <always0524@naver.com>
2025-08-13 23:34:54 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 46.98 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ').map(Number);
+}).on('close', function () {
+    const a = input[0];
+    const b = input[1];
+    const c = a + b;
+    console.log(`${a} + ${b} = ${c}`);
+});
