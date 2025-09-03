88d1a639392408eda724f2e5bffaf4eae926d8b4
minji-kim0524 <always0524@naver.com>
2025-09-03 22:40:46 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.js
@@ -0,0 +1,8 @@
+function solution(message) {
+    const LETTER_WIDTH_SIZE = 2
+    
+    const messageLength = message.length
+    const result = LETTER_WIDTH_SIZE * messageLength
+    
+    return result
+}
