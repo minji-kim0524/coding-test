0ca46a6fa4e3b467c29959b949eb870476cfa5a3
minji-kim0524 <always0524@naver.com>
2025-11-06 23:48:09 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -0,0 +1,11 @@
+function solution(n, numlist) {
+    const answer = []
+    
+    for (let i = 0; i < numlist.length; i++) {
+        if (numlist[i] % n === 0) {
+            answer.push(numlist[i])
+        }     
+    }
+    
+    return answer;
+}
