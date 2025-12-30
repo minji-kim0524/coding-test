0a7f3d896ac0091b7cb5ca6b6f91cf88d1b31d4b
김민지 <always0524@naver.com>
2025-12-30 21:49:23 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 27.42 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,14 +1,4 @@
 function solution(arr)
 {
-    var answer = []
-    
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== arr[i+1]) {
-            answer.push(arr[i])
-        } else if(i === arr.length -1) {
-            answer.push(arr[i])
-        }
-    }
-        
-    return answer
+    return arr.filter((val,index) => val != arr[index+1]);
 }
