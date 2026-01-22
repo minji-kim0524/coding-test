e8ceff61366c240db1cdf59b9503ca62cc14ba13
김민지 <always0524@naver.com>
2026-01-22 14:00:48 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,18 @@
+function solution(num) {
+    let count = 0
+    
+    if (num === 1) {
+        return 0
+    }
+    
+    while (num !== 1 && count < 500) {
+        if (num % 2 === 0) {
+            num = num / 2 
+        } else if (num % 2 !== 0) {
+            num = (num * 3) + 1
+        }
+        count++
+    }
+    return count === 500 ? -1 : count
+    
+}
