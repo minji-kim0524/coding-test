f77c0d73a3caed5716c18cc7dc5eff668ac8dc3e
김민지 <always0524@naver.com>
2026-02-19 21:34:02 +0900

    [level 2] Title: 짝지어 제거하기, Time: 67.97 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,13 +1,13 @@
-function solution(s) {
-    const stack = []
+function solution(s)
+{
 
-    for (let i = 0; i < s.length; i++) {
-        if (stack.length > 0 && stack[stack.length - 1] === s[i]) {
-            stack.pop();
-        } else {
-            stack.push(s[i]);
+    var walk = [];
+    for (var n of s) {
+        var last = walk.pop();
+        if (last != n) {
+            last && walk.push(last);
+            walk.push(n);
         }
     }
-
-    return stack.length === 0 ? 1 : 0;
+    return walk.length ? 0 : 1;
 }
