b4780c06d342d9c7e3793c592764a8ab8d92d789
김민지 <always0524@naver.com>
2026-02-19 20:44:30 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.17 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,19 +1,3 @@
-function solution(n) {
-
-    const binary = n.toString(2)    
-    let count = 0
-    for (let i = 0; i < binary.length; i++) {
-        if (binary[i] === '1') count++
-    }
-    
-    for (let next = n + 1; ; next++) {
-        let nextCount = 0
-        const nextBinary = next.toString(2)
-        
-        for (let j = 0; j < nextBinary.length; j++) {
-            if (nextBinary[j] === '1') nextCount++
-        }
-        
-        if (count === nextCount) return next
-    }
+function solution(n,a=n+1) {
+    return n.toString(2).match(/1/g).length == a.toString(2).match(/1/g).length ? a : solution(n,a+1);
 }
