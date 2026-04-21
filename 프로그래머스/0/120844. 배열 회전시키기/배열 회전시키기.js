4d0d6962c30f5b22625c9aeefccec2d8a0613324
김민지 <always0524@naver.com>
2026-04-21 23:34:20 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,11 @@
+function solution(numbers, direction) {
+    if (direction === "right") {
+        const last = numbers.pop()
+        numbers.unshift(last)
+    } else {
+        const first = numbers.shift()
+        numbers.push(first)
+    }
+    
+    return numbers
+}
